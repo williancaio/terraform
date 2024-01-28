b034b851bea821e7864c5faf79a68fd1996363d4
Caio Willian <caiow.willian@gmail.com>
2024-01-28 15:53:28 -0300

    chore: deploy ec2

--- ec2+ebs/ec2+ebs.tf
+++ ec2+ebs/ec2+ebs.tf
@@ -1,4 +1,17 @@
-resource "aws_instance" "teste" {
+data "aws_ami" "this" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "architecture"
+    values = ["arm64"]
+  }
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*"]
+  }
+}
+
+resource "aws_instance" "teste2" {
   ami           = var.ami
   subnet_id     = var.subnet_id
   instance_type = var.instance_type
